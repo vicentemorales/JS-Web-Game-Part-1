8095a588828a683b9536b671c6d6b65be59665fe
Vicente Morales <vicente.morales@HPPFS.local>
2023-03-01 08:07:25 -0800

    Added the first set of images

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function  newImage(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom +'px'
+    document.body.append(object)
+    return object
+}
+
+//Green Character
+newImage('assets/green-character.gif', 100, 100)
+
+//Pine Tree
+newImage('assets/pine-tree.png', 450, 200)
+
+//Tree
+newImage('assets/tree.png', 200, 300)
+
+//Pillar
+newImage('assets/pillar.png', 350, 100)
+
+//Crate
+newImage('assets/crate.png', 150, 200)
+
+//Well
+newImage('assets/well.png', 500, 425)
+
+//Sword
