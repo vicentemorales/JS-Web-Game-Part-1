32593852841395f64a878c9511e64abecc218724
Vicente Morales <vicente.morales@HPPFS.local>
2023-03-01 13:37:07 -0800

    added sword, shield, and staff

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ function  newImage(url, left, bottom){
     return object
 }
 
+
+// ---- Images ----
 //Green Character
 newImage('assets/green-character.gif', 100, 100)
 
@@ -26,4 +28,18 @@ newImage('assets/crate.png', 150, 200)
 //Well
 newImage('assets/well.png', 500, 425)
 
+// ---- Items ----
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
+    object.addEventListener('dblclick', () => {
+    object.remove()
+    })
+}
 //Sword
+newItem('assets/sword.png', 500, 405)
+
+newItem('assets/sheild.png', 165, 185)
+
+newItem('assets/staff.png', 600, 100)
+
+
